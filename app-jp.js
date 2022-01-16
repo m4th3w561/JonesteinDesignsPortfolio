6df83740a271f2b68935e2fd83e67a612e0b9fe9
Mathew_C <57735350+m4th3w561@users.noreply.github.com>
2022-01-16 14:16:16 +0900

    finished network effect

--- app-jp.js
+++ app-jp.js
@@ -1,13 +1,19 @@
 
 /* For jonestein-design-portfolio.html  */
 const loader = document.querySelector('.loader')
+const loaderContainer = document.querySelector(".loader-container")
 const body = document.querySelector('body')
 
 window.onload = () => {
   loader.style.width =  "0";
   loader.style.height = "0";
-  loader.style.transition = "2s ease-out"
+  loader.style.transition = "1.5s ease-out";
 }
+// window.onload = () => {
+//     loader.style.transitionDelay = "1.8s"
+//     loaderContainer.style.display = 'none';
+// }
+
 
 
 
